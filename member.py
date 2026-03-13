5b55e9e9fe0f4d6666e20aa4509b334187446a4a
Dian Fajar <dianfajarotp@gmail.com>
2026-03-13 11:46:27 +0700

    Add files via upload

---
+++ member.py
@@ -0,0 +1,21 @@
+class Member:
+    def __init__(self, id, nama, email):
+
+        if not isinstance(id, str):
+            raise TypeError("Id harus String")
+
+        if not isinstance(nama, str):
+            raise TypeError("Nama harus String")
+
+        if not isinstance(email, str):
+            raise TypeError("Email harus String")
+
+        if len(id) < 1:
+            raise ValueError("Id minimal 1 karakter")
+
+        if len(nama) < 3:
+            raise ValueError("Nama minimal 3 karakter")
+
+        self.__id = id
+        self.__nama = nama
+        self.__email = email
